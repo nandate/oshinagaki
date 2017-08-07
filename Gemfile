9b4fec6e9130f0bd24bddd0deacc47413da9c8b8
nandate <o238049t@mail.kyutech.jp>
2017-08-07 15:20:03 +0900

    twitter auth

--- Gemfile
+++ Gemfile
@@ -32,6 +32,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'twitter'
+gem 'omniauth'
+gem 'omniauth-twitter'
+
+gem 'dotenv-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
