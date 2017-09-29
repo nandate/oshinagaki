9777e1bd76bce9622fa300e596ee77915b3b8458
nandate <o238049t@mail.kyutech.jp>
2017-09-29 16:47:24 +0900

    authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user
+
   def show
     @user = User.find(params[:id])
     @menus = @user.menus
   end
+
+  private
+    def logged_in_user
+      unless logged_in?
+        redirect_to root_url
+      end
+    end
+
 end
