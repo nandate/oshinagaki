e80ac9211cc4550071dd1463bbcc4eb7225c99bf
nandate <o238049t@mail.kyutech.jp>
2017-10-14 19:31:10 +0900

    fix SessionsHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to user
+    redirect_back_or user
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user
 
   def show
     @user = User.find(params[:id])
@@ -9,8 +10,14 @@ class UsersController < ApplicationController
   private
     def logged_in_user
       unless logged_in?
-        redirect_to root_url
+        store_location
+        redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
     end
 
 end
